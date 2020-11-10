74576a980fcb8566ea2a79451bef48bd7b490b3c
Carlos Mora <carlosmora179@gmail.com>
2020-11-10 01:00:23 -0600

    Commit movimiento del jugador
    Tiene un bug al saltar que estoy trabajando

---
+++ Assets/Scripts/MovimientoPlayer.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MovimientoPlayer : MonoBehaviour
+{
+    
+    public float maxSpeed = 3.4f;
+    public float jumpHeight = 6.5f;
+    public float gravityScale = 1.5f;
+    public Camera mainCamera;
+
+    bool facingRight = true;
+    public float moveDirection = 0;
+
+    
+    public float dir ; 
+    public bool isGrounded = false;
+    Vector3 cameraPos;
+    Rigidbody2D r2d;
+    Collider2D mainCollider;
+    
+    LayerMask layerMask = ~(1 << 2 | 1 << 8);
+    Transform t;
+
+   
+    void Start()
+    {
+        t = transform;
+        r2d = GetComponent<Rigidbody2D>();
+        mainCollider = GetComponent<Collider2D>();
+        r2d.freezeRotation = true;
+        r2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        r2d.gravityScale = gravityScale;
+        facingRight = t.localScale.x > 0;
+        gameObject.layer = 8;
+
+        if(mainCamera)
+            cameraPos = mainCamera.transform.position;
+    }
+
+    
+    void Update()
+    {
+
+        
+        dir = r2d.velocity.x;
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && (isGrounded || r2d.velocity.x > 0.01f  ) )
+        {
+            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
+            
+
+            
+            //Debug.Log("gire");
+            
+        }
+        else
+        {
+            if (isGrounded || r2d.velocity.magnitude < 0.01f)
+            {
+                moveDirection = 0;
+                //aire = false;
+               
+            } 
+        }
+
+        
+        if (moveDirection != 0)
+        {
+            if (moveDirection > 0 && !facingRight)
+            {
+                facingRight = true;
+                t.localScale = new Vector3(Mathf.Abs(t.localScale.x), t.localScale.y, transform.localScale.z);
+            }
+            if (moveDirection < 0 && facingRight)
+            {
+                facingRight = false;
+                t.localScale = new Vector3(-Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
+            }
+        }
+
+        
+        if ((Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.Space)) && isGrounded)
+        {
+            Debug.Log("aca");
+            //aire = true;
+            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
+        }
+
+        
+        if(mainCamera)
+            mainCamera.transform.position = new Vector3(t.position.x, cameraPos.y, cameraPos.z);
+    }
+
+    void FixedUpdate()
+    {
+        Bounds colliderBounds = mainCollider.bounds;
+        Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, 0.1f, 0);
+        
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckPos, 0.23f, layerMask);
+
+        
+
+        
+
+        
+        r2d.velocity = new Vector2((moveDirection) * maxSpeed, r2d.velocity.y);
+
+    
+        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, 0.23f, 0), isGrounded ? Color.green : Color.red);
+    }
+}
