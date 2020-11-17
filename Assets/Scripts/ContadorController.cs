412599039ef4339dab70b4e8ac544dbcf1806856
KEVIN <kevin150598@gmail.com>
2020-11-17 13:45:31 -0600

    No se que pero se sube

---
+++ Assets/Scripts/ContadorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContadorController : MonoBehaviour
+{
+
+    public Text counterText;
+    public GameObject player;
+    
+    float cronometro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counterText = GetComponent<Text>() as Text;
+        player = GameObject.Find("Player1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        cronometro = player.GetComponent<colorController>().cronometro;
+        
+        counterText.text = cronometro.ToString("00");
+    }
+    private void FixedUpdate() {
+        counterText.color = player.GetComponent<Renderer>().material.color;
+    }
+}
---
+++ Assets/Scripts/ControlesMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlesMenu : MonoBehaviour
+{
+     [SerializeField] Text controles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        controles.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C)){
+            Debug.Log(controles.enabled);
+            controles.enabled = true;
+        }
+        
+        if(Input.GetKeyUp(KeyCode.C)){
+            controles.enabled = false;
+        }
+    }
+}
