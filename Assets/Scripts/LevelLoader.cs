35c7ea57007afbe0fceefdbe5beb81f336d0181b
Carlos Mora <carlosmora179@gmail.com>
2020-11-18 21:20:49 -0600

    Transicion de mundos y menu principal

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+   public Animator transition;
+
+   public GameObject canva;
+
+   public float transitionTime = 1f;
+    // Update is called once per frame
+   private void Start() {
+        StartCoroutine(loadInterface());
+       
+   }
+
+    public void LoadNextLevel(){
+
+        
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+         
+
+
+    }
+    IEnumerator LoadLevel(int levelIndex){
+        //play
+            transition.SetTrigger("Start");
+        //wait
+        yield return new WaitForSeconds(transitionTime );
+
+        //load Scene
+        SceneManager.LoadScene(levelIndex);
+
+
+    }
+
+   IEnumerator loadInterface()
+  {
+       
+       yield return new WaitForSeconds(transitionTime);
+       canva.SetActive(true);
+  }
+}
