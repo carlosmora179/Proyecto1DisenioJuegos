5e8c734a153ee07c1a26c739936bf5da51ee149a
JGatgens <jorgee64@gmail.com>
2020-11-19 15:47:53 -0600

    Agregando plataforma movible sin asset

---
+++ Assets/Scripts/PlataformaMovible.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaMovible : MonoBehaviour
+{
+    public Renderer rend;
+    public Rigidbody2D rb2;
+    public GameObject orbe;
+    private Color color;
+    private float cronometro;
+    private bool salir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = orbe.GetComponent<Orbe>().color;
+        rend.material.color = color;
+        cronometro = 1;
+        salir = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Renderer>() != null)
+        {
+            if (collision.collider.GetComponent<Renderer>().material.color == color)
+            {
+                rb2.constraints &= ~RigidbodyConstraints2D.FreezePositionX;
+                salir = false;
+                cronometro = 1;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        salir = true;
+        
+    }
+
+    private void Update()
+    {
+        if (salir)
+        {
+            if (cronometro < 0)
+            {
+                rb2.constraints |= RigidbodyConstraints2D.FreezePositionX;
+                cronometro = 1;
+                salir = false;
+            }
+            else
+            {
+                cronometro -= Time.deltaTime;
+            }
+        }
+    }
+}
