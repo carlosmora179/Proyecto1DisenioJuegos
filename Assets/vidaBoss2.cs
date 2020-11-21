ea60c89bc3fc9bdf5b41949d806f09d6a0c82a28
Carlos Mora <carlosmora179@gmail.com>
2020-11-20 19:18:35 -0600

    comit de todo

---
+++ Assets/vidaBoss2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vidaBoss2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public  int maxHealth = 100;
+    int currentHealth;
+    
+     // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int damage){
+        currentHealth -= damage;
+        GetComponent<Boss1Movement>().Mover();
+        if(currentHealth <=0){
+            Die();
+        }
+    }
+    void Die(){
+        Debug.Log("Oso Morido");
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+   
+}
